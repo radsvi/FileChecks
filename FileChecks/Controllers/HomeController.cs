252fb65b5c4e150d1e4d83fa637b6a5da96e4bdc
Radomir Svihel <189124653+radsvi@users.noreply.github.com>
2026-02-03 13:12:18 +0100

    Removing FolderController and moving everything to HomeController

--- FileChecks/Controllers/HomeController.cs
+++ FileChecks/Controllers/HomeController.cs
@@ -7,10 +7,31 @@ namespace FileChecks.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IVersionManagerFactory _factory;
+        public HomeController(IVersionManagerFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [HttpPost]
+        public IActionResult Index(string? subFolderPath)
+        {
+            var versionManager = _factory.Create();
+
+            if (ModelState.IsValid)
+            {
+                versionManager.Start(subFolderPath);
+            }
+
+            return View(versionManager);
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var versionManager = _factory.Create();
+            //versionManager.Start(string.Empty);
+
+            return View(versionManager);
         }
     }
 }
